bcc2b5b626764627c494ed55f1b3d49eeebce0ea
LoneHandyman <78569671+LoneHandyman@users.noreply.github.com>
2021-02-11 20:41:37 -0500

    Only neuron testing

--- src/Neuron.hpp
+++ src/Neuron.hpp
@@ -3,32 +3,51 @@
 
 #include "TransferFunction.hpp"
 
+#include <chrono>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <vector>
 
 namespace nn{
+  std::default_random_engine random(std::chrono::system_clock::now().time_since_epoch().count());
   struct Neuron{
     Neuron(std::size_t inputs, const std::function<double(double)>& activation){
+      std::uniform_real_distribution<double> distribution(-1.0,1.0);
       ptrTransferFunc = std::make_shared<TransferFunction>(activation);
       inputWeights.resize(inputs);
+      numOfDentrites = inputs;
       for(double& val : inputWeights){
-        val = static_cast<double>(rand() / RAND_MAX);
+        val = distribution(random);
       }
+      bias = distribution(random);
+    }
+
+    void printInfo(){
+      std::cout << "Weights:" << std::endl;
+      for(double& val : inputWeights){
+        std::cout << '\t' << val << std::endl;
+      }
+      std::cout << "Bias: " << bias << std::endl;
     }
 
     void onProcessing(const std::vector<double>& inputValues){
-      
+      outPutValue = 0.0;
+      for(std::size_t idx = 0; idx < numOfDentrites; ++idx){
+        outPutValue += inputValues[idx] * inputWeights[idx];
+      }
     }
 
-    void onSynapsis(){
-      std::cout << "Transfer Function: " << (*ptrTransferFunc)(-1.2) << std::endl;
-      std::cout << "Desc. Gradient Function: " << ptrTransferFunc->d$(-1.2) << std::endl;
+    double onSynapsis(){
+      //std::cout << "Transfer Function: " << (*ptrTransferFunc)(-1.2) << std::endl;
+      //std::cout << "Desc. Gradient Function: " << ptrTransferFunc->d$(-1.2) << std::endl;
+      return (*ptrTransferFunc)(outPutValue);
     }
 
   private:
     std::shared_ptr<TransferFunction> ptrTransferFunc;
     std::vector<double> inputWeights;
+    std::size_t numOfDentrites;
     double outPutValue, bias;
   };
 }
--- src/demo.cpp
+++ src/demo.cpp
@@ -1,7 +1,10 @@
 #include "NeuralNetwork.hpp"
 
 int __cdecl main(){
-  nn::Neuron new_neuron(nn::predef_functions::sigmoid);
-  new_neuron.onSynapsis();
+  nn::Neuron new_neuron(5, nn::predef_functions::sigmoid);
+  new_neuron.printInfo();
+  std::vector<double> data = {0.12,2.3,0.2,0.22,0.05};
+  new_neuron.onProcessing(data);
+  std::cout << new_neuron.onSynapsis() << std::endl;
   return EXIT_SUCCESS;
 }
